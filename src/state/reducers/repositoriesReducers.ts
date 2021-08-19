c78d1e41c9b3fc709d25c73e8f8d72436e962307
tmm-michalski <tmm.michalski@gmail.com>
2021-08-19 11:04:48 +0200

    Add action interfaces.

--- src/state/reducers/repositoriesReducers.ts
+++ src/state/reducers/repositoriesReducers.ts
@@ -4,24 +4,42 @@ interface RepositoriesState {
     data: string[];
 }
 
+interface SearchRepositoriesAction {
+    type: 'search_repositories';
+}
+
+interface SearchRepositoriesSuccessAction {
+    type: 'search_repositories_success';
+    payload: string[];
+}
+
+interface SearchRepositoriesErrorAction {
+    type: 'search_repositories_error';
+    payload: string;
+}
+
 export const reducer = (
     state: RepositoriesState,
-    action: any
+    action:
+        | SearchRepositoriesAction
+        | SearchRepositoriesSuccessAction
+        | SearchRepositoriesErrorAction
 ): RepositoriesState => {
+    // Remember about "type guards".
     switch (action.type) {
-        case "search_repositories":
+        case 'search_repositories':
             return {
                 loading: true,
                 error: null,
                 data: [],
             };
-        case "search_repositories_success":
+        case 'search_repositories_success':
             return {
                 loading: false,
                 error: null,
                 data: action.payload,
             };
-        case "search_repositories_error":
+        case 'search_repositories_error':
             return {
                 loading: false,
                 error: action.payload,
